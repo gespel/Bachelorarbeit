b1c7ce398b8501229de79097f5657fe7a4d45591
Max Schrötter <max@schroetter.info>
2025-04-09 15:54:02 +0200

    server refresh

--- tools/dns-server.py
+++ tools/dns-server.py
@@ -2,10 +2,10 @@ import socket
 from scapy.all import DNS, DNSQR, DNSRR
 import logging
 
-LISTEN_IP = "0.0.0.0"
+LISTEN_IP = "10.3.10.45"
 LISTEN_PORT = 53
 TARGET_DOMAINS = ["example.local.", "example.com.", "example.org.", "test.local."]
-REPLY_IP = "192.168.1.101"
+REPLY_IP = "10.3.10.43"
 
 logging.basicConfig(level=logging.INFO)
 
@@ -17,7 +17,7 @@ logging.info(f"DNS-Server läuft auf {LISTEN_IP}:{LISTEN_PORT} für {TARGET_DOMA
 while True:
     data, addr = sock.recvfrom(512)
     dns = DNS(data)
-
+    logging.info(f"Paket erhalten von {addr}")
     if dns.opcode == 0 and dns.qr == 0 and dns.qdcount == 1:
         qname = dns[DNSQR].qname.decode()
         logging.info(f"Anfrage: {qname} von {addr}")
@@ -28,10 +28,10 @@ while True:
                 qr=1,
                 aa=1,
                 qd=dns.qd,
-                an=DNSRR(rrname=qname, ttl=60, type="A", rclass="IN", rdata=REPLY_IP)
+                an=DNSRR(rrname=qname, ttl=60, type="A", rclass="IN", rdata="1.1.1.1")
             )
 
-            sock.sendto(bytes(response), addr)
-            logging.info(f"Antwort gesendet: {qname} -> {REPLY_IP}")
+            sock.sendto(bytes(response), (REPLY_IP, 5353))
+            logging.info(f"Antwort gesendet: {qname} -> 1.1.1.1")
         else:
             logging.info("Nicht die gewünschte Domain")
--- tools/pps.sh
+++ tools/pps.sh
@@ -3,9 +3,9 @@ IFACE=ens1f0np0
 INTERVAL=1
 
 while true; do
-    START=$(ethtool -S "$IFACE" | grep rx_packets | awk '{print $2}')
+    START=$(ethtool -S "$IFACE" | grep 'rx_packets_phy:' | awk '{print $2}' | tr -d '\r')
     sleep "$INTERVAL"
-    END=$(ethtool -S "$IFACE" | grep rx_packets | awk '{print $2}')
+    END=$(ethtool -S "$IFACE" | grep 'rx_packets_phy:' | awk '{print $2}' | tr -d '\r')
     PPS=$((END - START))
-    echo "PPS: $PPS"
+    echo $PPS
 done
