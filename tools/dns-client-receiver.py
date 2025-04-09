e76da227caae97b1122124972906793267f1bf66
gespel <heimbrodt@uni-potsdam.de>
2025-04-09 14:03:48 +0200

    sender and receiver seperate now

---
+++ tools/dns-client-receiver.py
@@ -0,0 +1,24 @@
+import time
+from scapy.all import sniff, DNS, UDP
+
+INTERFACE = "enp24s0f0np0"
+SPORT = 5353 
+
+def match_packet(pkt):
+    return (
+        pkt.haslayer(DNS)
+        and pkt.haslayer(UDP)
+        and pkt[DNS].qr == 1
+        and pkt[UDP].dport == SPORT
+    )
+
+print(f"[Receiver] Warte auf DNS-Antwort auf Interface {INTERFACE} ...")
+
+pkt = sniff(iface=INTERFACE, lfilter=match_packet, timeout=2, count=1)
+
+if pkt:
+    #rtt_ms = (time.time() - sent_time) * 1000
+    dns_resp = pkt[0][DNS]
+    print(f"[Receiver] Antwort empfangen: {dns_resp.an.rdata} | time: {time.time()}")
+else:
+    print("[Receiver] Keine Antwort empfangen.")
---
+++ tools/dns-client-sender.py
@@ -0,0 +1,30 @@
+import time
+import random
+from scapy.all import DNS, DNSQR, Ether, IP, UDP, sendp, get_if_hwaddr
+
+INTERFACE = "enp24s0f0np0"
+TARGET_MAC = "c4:70:bd:a0:56:ac"
+SOURCE_MAC = get_if_hwaddr(INTERFACE)
+TARGET_IP = "10.3.10.45"
+SOURCE_IP = "10.3.10.42"
+TARGET_PORT = 53
+
+QUERY_DOMAINS = ["example.local.", "example.org.", "test.local."]
+
+DNS_TXID = 0x1234  # feste ID
+SPORT = 44444      # feste Source-Port für einfaches Matchen
+
+def send_dns_request(domain):
+    dns_request = DNS(id=DNS_TXID, rd=1, qd=DNSQR(qname=domain))
+    ether = Ether(src=SOURCE_MAC, dst=TARGET_MAC)
+    ip = IP(src=SOURCE_IP, dst=TARGET_IP)
+    udp = UDP(sport=SPORT, dport=TARGET_PORT)
+    packet = ether / ip / udp / dns_request
+
+    sendp(packet, iface=INTERFACE, verbose=0)
+
+if __name__ == "__main__":
+    domain = random.choice(QUERY_DOMAINS)
+    print(f"[Sender] Sende Anfrage für {domain}")
+    start = time.time()
+    send_dns_request(domain)
