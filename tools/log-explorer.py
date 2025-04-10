760028aa5942187c567fb4700f89393f33fc90c8
gespel <heimbrodt@uni-potsdam.de>
2025-04-10 18:57:15 +0200

    ka

--- tools/log-explorer.py
+++ tools/log-explorer.py
@@ -3,33 +3,42 @@ import sys
 
 logfile = sys.argv[1]
 
+time_format = "%H:%M:%S.%f"
+
 requests = []
 responses = []
-
-time_format = "%H:%M:%S.%f"
 
 with open(logfile, "r") as f:
     for line in f:
         line = line.strip()
         if not line:
             continue
-
         timestamp = line.split()[0]
         if "4660+" in line:
             requests.append(datetime.strptime(timestamp, time_format))
         elif "4660*-" in line:
             responses.append(datetime.strptime(timestamp, time_format))
 
-rtts = []
-paired_count = min(len(requests), len(responses))
+used_responses = set()
+matched_pairs = []
 
-for i in range(paired_count):
-    rtt = (responses[i] - requests[i]).total_seconds() * 1000
-    rtts.append(rtt)
+for req_time in requests:
+    pair = None
+    for i, resp_time in enumerate(responses):
+        if i in used_responses:
+            continue
+        if resp_time > req_time:
+            pair = (req_time, resp_time)
+            used_responses.add(i)
+            break
+    if pair:
+        matched_pairs.append(pair)
 
-lost_requests = len(requests) - paired_count
-successful_requests = paired_count
+rtts = [(resp - req).total_seconds() * 1000 for req, resp in matched_pairs]  # in ms
+
 total_requests = len(requests)
+successful = len(matched_pairs)
+lost = total_requests - successful
 
 for i, rtt in enumerate(rtts, 1):
     print(f"RTT #{i}: {rtt:.3f} ms")
@@ -39,5 +48,5 @@ if rtts:
     print(f"\nDurchschnittliche RTT: {avg_rtt:.3f} ms")
 
 print(f"\nAnzahl Anfragen insgesamt: {total_requests}")
-print(f"Erfolgreiche Antworten: {successful_requests}")
-print(f"Verlorene Pakete (keine Antwort): {lost_requests}")
+print(f"Erfolgreiche Antworten: {successful}")
+print(f"Verlorene Pakete (keine Antwort): {lost}")
