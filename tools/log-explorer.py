efaacfb021350de42a6f59424ca660753da0e897
gespel <heimbrodt@uni-potsdam.de>
2025-04-13 12:09:07 +0200

    ignore rtt > 1000 ms

--- tools/log-explorer.py
+++ tools/log-explorer.py
@@ -23,7 +23,8 @@ while i < len(lines):
             if "4660*-" in next_line:
                 response_time = datetime.strptime(next_line.split()[0], time_format)
                 rtt = (response_time - request_time).total_seconds() * 1000
-                rtts.append(rtt)
+                if rtts < 1000:
+                    rtts.append(rtt)
                 i += 2
                 continue
         
