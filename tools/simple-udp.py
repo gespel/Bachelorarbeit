d51389bc1b01dded3dd278fa462429aeeb858fcc
gespel <heimbrodt@uni-potsdam.de>
2025-09-05 22:07:48 +0200

    actual simple udp

--- tools/simple-udp.py
+++ tools/simple-udp.py
@@ -1,17 +1,18 @@
 from scapy.all import Ether, IP, UDP, DNS, DNSQR, sendp
 from tqdm import tqdm
 import time
+import sys
 
 # MAC-Adressen
 dst_mac = "c4:70:bd:a0:56:ac" # bluefield
 # dst_mac = "a0:88:c2:b6:14:1a" # fips1
 #dst_mac = "a0:88:c2:b5:f4:5a" # fips2
 # dst_mac = "c4:70:bd:a0:56:ac"  # aktuell verwendete Ziel-MAC
-src_mac = "08:c0:eb:a5:61:26"   # deine Quell-MAC
+src_mac = "a0:88:c2:b6:14:1a"   # deine Quell-MAC
 
 # IP-Adressen und Ports
 src_ip = "1.2.3.4"
-dst_ip = "192.168.1.100"
+dst_ip = "10.3.10.45"
 src_port = 12345
 dst_port = 53  # DNS-Port
 
@@ -33,7 +34,12 @@ interval = 0.2            # 0,2 Sekunden
 # Berechne Anzahl der Pakete
 num_packets = int(duration_sec / interval)
 
+if "--infinite" in sys.argv:
+    while True:
+        sendp(pkt, iface="enp24s0f0np0", verbose=False)
+        time.sleep(interval)
+else:
 # Pakete mit Fortschrittsbalken senden
-for _ in tqdm(range(num_packets), desc="Sende DNS-Pakete"):
-    sendp(pkt, iface="enp24s0f0np0", verbose=False)
-    time.sleep(interval)
+    for _ in tqdm(range(num_packets), desc="Sende DNS-Pakete"):
+        sendp(pkt, iface="enp24s0f0np0", verbose=False)
+        time.sleep(interval)
